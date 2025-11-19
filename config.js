b7bcff23bee0575cbb5b17a2a1fd5e53c6ad7bb9
thedg17 <d.garach17@gmail.com>
2025-11-20 01:48:29 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+// ⬅️ REPLACE WITH YOUR VALUES
+const GOOGLE_CLIENT_ID = "813185029300-ej2m0qrpae9kucc0b2b89phu1l2poc7u.apps.googleusercontent.com";
+const GOOGLE_API_KEY = "AIzaSyDjeOD1iJJ16hDIG0_gLBpPCTmqzW0KxAk";
+const SHEET_ID = "/d/1Vp3kqDAZNHwYD-AHiYjM5ry646Bd6J5GhKFUe71J7hA/edit?gid=1843599820#gid=1843599820";
+
+// read + write access
+const SCOPES = "https://www.googleapis.com/auth/spreadsheets";
