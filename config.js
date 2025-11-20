bb0a6b92ab3aa6a6741e8adca970744e1dc8fb79
thedg17 <d.garach17@gmail.com>
2025-11-20 12:37:45 +0530

    config.js

--- config.js
+++ config.js
@@ -1,8 +1,15 @@
-// ⬅️ REPLACE WITH YOUR VALUES
-const GOOGLE_CLIENT_ID = "813185029300-ej2m0qrpae9kucc0b2b89phu1l2poc7u.apps.googleusercontent.com";
-const GOOGLE_API_KEY = "AIzaSyDjeOD1iJJ16hDIG0_gLBpPCTmqzW0KxAk";
-const SHEET_ID = "/d/1Vp3kqDAZNHwYD-AHiYjM5ry646Bd6J5GhKFUe71J7hA/edit?gid=1843599820#gid=1843599820";
+// ================================
+// GOOGLE API CONFIG
+// ================================
 
-// read + write access
+// Your Google Cloud API Key
+const GOOGLE_API_KEY = "AIzaSyDjeOD1iJJ16hDIG0_gLBpPCTmqzW0KxAk";
+
+// Your OAuth Client ID (Web client)
+const GOOGLE_CLIENT_ID = "813185029300-ej2m0qrpae9kucc0b2b89phu1l2poc7u.apps.googleusercontent.com";
+
+// Sheets access scope
 const SCOPES = "https://www.googleapis.com/auth/spreadsheets";
 
+// Your Google Sheet ID
+const SHEET_ID = "1Vp3kqDAZNHwYD-AHiYjM5ry646Bd6J5GhKFUe71J7h";
